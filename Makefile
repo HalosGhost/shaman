48f35321503e4079e6bab6035effd08097d6e9e9
HalosGhost <halosghost@archlinux.info>
2013-12-28 13:36:47 -0600

    Standardize tabstops; add makefile file dependencies for rules

--- Makefile
+++ Makefile
@@ -1,23 +1,25 @@
-CFLAGS	+=	-Os -Wall
-PROG		=	shaman
-PREFIX	?=	/usr
-VER		=	0.1
-MANDIR	?=	/share/man
-LIBS		= -lm -lcurl -lxml2
-INCLUDE	= -I/usr/include/libxml2
+CFLAGS  +=  -Os -Wall
+PROG    =   shaman
+PREFIX  ?=  /usr
+VER     =   0.1
+MANDIR  ?=  /share/man
+LIBS    =   -lm -lcurl -lxml2
+INCLUDE =   -I/usr/include/libxml2
 
 ${PROG}: ${PROG}.c
-	@${CC} ${INCLUDE} ${CFLAGS} ${LIBS} -o ${PROG} ${PROG}.c
-#	@strip ${PROG}
+    @${CC} ${INCLUDE} ${CFLAGS} ${LIBS} -o ${PROG} ${PROG}.c
+#   @strip ${PROG}
 
-clean:
-	@rm -f ${PROG}
-	@rm -f ${PROG}.1
+clean: ${PROG} ${PROG}.1
+    @rm -f ${PROG}
+    @rm -f ${PROG}.1
 
-docs:
-	@latex2man man1.tex ${PROG}.1
+docs: man1.tex
+    @latex2man man1.tex ${PROG}.1
 
-install:
-	@install -Dm755 ${PROG} ${DESTDIR}${PREFIX}/bin/${PROG}
-	@install -Dm644 ${PROG}.1 ${DESTDIR}${PREFIX}${MANDIR}/man1/${PROG}.1
-	@install -Dm644 _${PROG} ${DESTDIR}${PREFIX}/share/zsh/site-functions/_${PROG}
+install: ${PROG}
+    @install -Dm755 ${PROG} ${DESTDIR}${PREFIX}/bin/${PROG}
+    @install -Dm644 ${PROG}.1 ${DESTDIR}${PREFIX}${MANDIR}/man1/${PROG}.1
+    @install -Dm644 _${PROG} ${DESTDIR}${PREFIX}/share/zsh/site-functions/_${PROG}
+
+# vim: set tabstop=4 shiftwidth=4 expandtab
--- shaman.c
+++ shaman.c
@@ -23,8 +23,8 @@ char passLoc[BUFFER_SIZE] = {'\0'};
 
 typedef struct
 {   char * buffer;
-	size_t size;
-	size_t offset;
+    size_t size;
+    size_t offset;
 }data_t;
 
 // Prototypes //
@@ -41,119 +41,119 @@ static void _formatOutput(char * formatStr);
 // Main Function //
 int main(int argc, char ** argv)
 {   static int flag_help;
-	static int flag_metric;
+    static int flag_metric;
 
-	if ( argc > 1 )
-	{   int c;
+    if ( argc > 1 )
+    {   int c;
+        
+        while (1)
+        {   static struct option long_options[] =
+            {   // Long Option Flags //
+                {"help",      no_argument,         0,    'h'   },
+                {"imperial",  no_argument,         0,    'i'   },
+                {"metric",    no_argument,         0,    'm'   },
+                // Long Option Switches //
+                {"format",    required_argument,   0,    'f'   },
+                {"location",  required_argument,   0,    'l'   },
+                {0,           0,                   0,    0     },
+            };
 
-		while (1)
-		{	static struct option long_options[] =
-			{   // Long Option Flags //
-				{"help",	  no_argument,		   0,	 'h'   },
-				{"imperial",  no_argument,		   0,	 'i'   },
-				{"metric",	  no_argument,		   0,	 'm'   },
-				// Long Option Switches //
-				{"format",	  required_argument,   0,	 'f'   },
-				{"location",  required_argument,   0,	 'l'   },
-				{0,			  0,				   0,	 0	   },
-			};
+            int option_index = 0; // Stores the option index
 
-			int option_index = 0; // Stores the option index
+            c = getopt_long(argc, argv, "hif:l:m", long_options, &option_index);
 
-			c = getopt_long(argc, argv, "hif:l:m", long_options, &option_index);
+            if ( c == -1 ) break;
 
-			if ( c == -1 ) break;
+            switch (c)
+            {   case 'h':
+                    flag_help = 1;
+                    break;
 
-			switch (c)
-			{   case 'h':
-				    flag_help = 1;
-					break;
+                case 'i':
+                    flag_metric = 0;
+                    break;
 
-			    case 'i':
-					flag_metric = 0;
-					break;
+                case 'f':
+                    snprintf(formatString, sizeof(formatString), "%s", optarg);
+                    break;
 
-			    case 'f':
-					snprintf(formatString, sizeof(formatString), "%s", optarg);
-					break;
+                case 'l':
+                    snprintf(passLoc, sizeof(passLoc), "%s", optarg);
+                    break;
 
-			    case 'l':
-					snprintf(passLoc, sizeof(passLoc), "%s", optarg);
-					break;
+                case 'm':
+                    flag_metric = 1;
+                    break;
+            }
+        }
+    }
 
-			    case 'm':
-					flag_metric = 1;
-					break;
-			}
-		}
-	}
+    if ( flag_help == 1 ) _usage();
 
-	if ( flag_help == 1 ) _usage();
-
-	if ( *passLoc ) // Inspiration: http://www.geekhideout.com/urlcode.shtml
+    if ( *passLoc ) // Inspiration: http://www.geekhideout.com/urlcode.shtml
     {   char * end;
-		const long sl = strtol(passLoc, &end, 10);
+        const long sl = strtol(passLoc, &end, 10);
 
-		if ( end == passLoc )
-	    {	char * pass = passLoc;
-			char * buffer = _malloc(strlen(passLoc) * 3 + 1);
-			char * buff = buffer;
+        if ( end == passLoc )
+        {   char * pass = passLoc;
+            char * buffer = _malloc(strlen(passLoc) * 3 + 1);
+            char * buff = buffer;
 
-			while ( *pass )
-			{   if ( isalnum(*pass) ) *buff++ = *pass;
-				else
-			    {   switch (*pass)
-			        {   case '-':
-						case '.':
-						case ',':
-							*buff++ = *pass;
-							break;
+            while ( *pass )
+            {   if ( isalnum(*pass) ) *buff++ = *pass;
+                else
+                {   switch (*pass)
+                    {   case '-':
+                        case '.':
+                        case ',':
+                            *buff++ = *pass;
+                            break;
 
-						case ' ':
-							*buff++ = '+';
-							break;
-					}
-				}
-				pass++;
-			}
+                        case ' ':
+                            *buff++ = '+';
+                            break;
+                    }
+                }
+                pass++;
+            }
 
-			*buff = '\0';
-			_getData(buffer, flag_metric);
-			free(buffer);
-		}
-		else if ( sl > 99999 || sl < 00000 )
-	    {	fprintf(stderr, "Invalid zip code: %ld\n", sl);
-			exit(1);
-		}
-		else _getData((char * )passLoc, flag_metric);
-	}
-	else
+            *buff = '\0';
+            _getData(buffer, flag_metric);
+            free(buffer);
+        }
+        else if ( sl > 99999 || sl < 00000 )
+        {   fprintf(stderr, "Invalid zip code: %ld\n", sl);
+            exit(1);
+        }
+        else _getData((char * )passLoc, flag_metric);
+    }
+    else
     {   fprintf(stderr, "No specified location\n");
-		exit(1);
-	}
+        exit(1);
+    }
 
-	// Call other functions for parsing
+    // Call other functions for parsing
 
-	return 0;
+    return 0;
 }
 
 // Memory Safety //
 void * _malloc(size_t size)
 {   void * e = malloc(size);
-	if ( e ) return e;
-	else return _memoryAbort();
+    if ( e ) return e;
+    else return _memoryAbort();
 }
 
 void * _realloc(void * ptr, size_t size)
 {   void * e = realloc(ptr, size);
-	if ( e ) return e;
-	else return _memoryAbort();
+    if ( e ) return e;
+    else return _memoryAbort();
 }
 
 void * _memoryAbort(void)
-{	puts("Failed to allocate memory");
-	exit(1);
-	return NULL;
+{   puts("Failed to allocate memory");
+    exit(1);
+    return NULL;
 }
 
 // Usage //
@@ -167,212 +167,213 @@ Options:\n\
   -f, --format=\"FORMAT\"         format output according to \"FORMAT\"\n\
   -l, --location=\"location\"     print weather information for \"location\"\n\n\
 See `man 1 shaman` for more information\n", stderr);
-	exit(0);
+  exit(0);
 }
 
 // Data and Analysis Functions //
 void _getData(const char * location, const int scale)
 {   CURL * handle;
-	CURLcode res;
-	char url[256] = "";
-	FILE * suppressOutput = fopen("/dev/null", "wb");
+    CURLcode res;
+    char url[256] = "";
+    FILE * suppressOutput = fopen("/dev/null", "wb");
 
-	data_t data;
-	memset(&data, '\0', sizeof(data_t));
+    data_t data;
+    memset(&data, '\0', sizeof(data_t));
 
-	curl_global_init(CURL_GLOBAL_ALL);
-	handle = curl_easy_init();
+    curl_global_init(CURL_GLOBAL_ALL);
+    handle = curl_easy_init();
 
-	if (handle)
+    if (handle)
     {   snprintf(url, sizeof(url), "http://forecast.weather.gov/zipcity.php?inputstring=%s", location);
-		curl_easy_setopt(handle, CURLOPT_WRITEDATA, suppressOutput);
-		curl_easy_setopt(handle, CURLOPT_URL, url);
-		curl_easy_setopt(handle, CURLOPT_FOLLOWLOCATION, 1);
+        curl_easy_setopt(handle, CURLOPT_WRITEDATA, suppressOutput);
+        curl_easy_setopt(handle, CURLOPT_URL, url);
+        curl_easy_setopt(handle, CURLOPT_FOLLOWLOCATION, 1);
 
-		res = curl_easy_perform(handle);
-		if ( res == CURLE_OK )
-	    {	char * interim;
-			res = curl_easy_getinfo(handle, CURLINFO_EFFECTIVE_URL, &interim);
+        res = curl_easy_perform(handle);
+        if ( res == CURLE_OK )
+        {   char * interim;
+            res = curl_easy_getinfo(handle, CURLINFO_EFFECTIVE_URL, &interim);
 
-			snprintf(url, sizeof(url), "%s&FcstType=dwml&unit=%d", interim, scale);
+            snprintf(url, sizeof(url), "%s&FcstType=dwml&unit=%d", interim, scale);
 
-			if ( res == CURLE_OK )
-			{   curl_easy_setopt(handle, CURLOPT_URL, url);
-				curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, _writeDataToBuffer);
-				curl_easy_setopt(handle, CURLOPT_WRITEDATA, (void * )&data);
-				res = curl_easy_perform(handle);
-			}
-			else
-			{   fprintf(stderr, "Failed to get weather page (%s)\n", curl_easy_strerror(res));
-				fclose(suppressOutput);
-				curl_easy_cleanup(handle);
-				curl_global_cleanup();
-				free(data.buffer);
-				exit(1);
-			}
-		}
-		else
-	    {	fprintf(stderr, "Failed to get location (%s)\n", curl_easy_strerror(res));
-			fclose(suppressOutput);
-			curl_easy_cleanup(handle);
-			curl_global_cleanup();
-			free(data.buffer);
-			exit(1);
-		}
-	}
+            if ( res == CURLE_OK )
+            {   curl_easy_setopt(handle, CURLOPT_URL, url);
+                curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, _writeDataToBuffer);
+                curl_easy_setopt(handle, CURLOPT_WRITEDATA, (void * )&data);
+                res = curl_easy_perform(handle);
+            }
+            else
+            {   fprintf(stderr, "Failed to get weather page (%s)\n", curl_easy_strerror(res));
+                fclose(suppressOutput);
+                curl_easy_cleanup(handle);
+                curl_global_cleanup();
+                free(data.buffer);
+                exit(1);
+            }
+        }
+        else
+        {   fprintf(stderr, "Failed to get location (%s)\n", curl_easy_strerror(res));
+            fclose(suppressOutput);
+            curl_easy_cleanup(handle);
+            curl_global_cleanup();
+            free(data.buffer);
+            exit(1);
+        }
+    }
 
-	fclose(suppressOutput);
-	curl_easy_cleanup(handle);
-	curl_global_cleanup();
+    fclose(suppressOutput);
+    curl_easy_cleanup(handle);
+    curl_global_cleanup();
 
-	// Uncomment the following line to see the fetched file
-	//puts(data.buffer);
+    // Uncomment the following line to see the fetched file
+    //puts(data.buffer);
 
-	_parseDataInBuffer(data.buffer, data.size);
+    _parseDataInBuffer(data.buffer, data.size);
 
-	free(data.buffer);
+    free(data.buffer);
 
-	if ( *formatString ) _formatOutput(formatString);
-	else
+    if ( *formatString ) _formatOutput(formatString);
+    else
     {   fprintf(stderr, "No specified format\n");
-		exit(1);
-	}
+        exit(1);
+    }
 }
 
 size_t _writeDataToBuffer(char * ptr, size_t size, size_t nmemb, void * userdata)
 {   data_t * data = (data_t * )userdata;
-	size_t s = size * nmemb;
+    size_t s = size * nmemb;
 
-	data->size += s;
-	data->buffer = _realloc(data->buffer, data->size + 1);
-	data->buffer[data->size] = '\0';
+    data->size += s;
+    data->buffer = _realloc(data->buffer, data->size + 1);
+    data->buffer[data->size] = '\0';
 
-	if ( ptr )
+    if ( ptr )
     {   memcpy(data->buffer + data->offset, ptr, s);
-		data->offset += s;
-	}
+        data->offset += s;
+    }
 
-	return s;
+    return s;
 }
 
 void _parseDataInBuffer(const char * buffer, int buffer_size)
 {   xmlDocPtr weather;
-	xmlNodePtr cur;
+    xmlNodePtr cur;
 
-	weather = xmlParseMemory(buffer, buffer_size);
+    weather = xmlParseMemory(buffer, buffer_size);
 
-	if ( !weather )
+    if ( !weather )
     {   fprintf(stderr, "Failed to parse weather data\n");
-		exit(1);
-	}
+        exit(1);
+    }
 
-	cur = xmlDocGetRootElement(weather);
+    cur = xmlDocGetRootElement(weather);
 
-	if ( !cur )
+    if ( !cur )
     {   fprintf(stderr, "Weather file is empty\n");
-		xmlFreeDoc(weather);
-		exit(1);
-	}
+        xmlFreeDoc(weather);
+        exit(1);
+    }
 
-	if ( xmlStrcmp((const xmlChar * )cur->name, (const xmlChar * )"dwml") )
+    if ( xmlStrcmp((const xmlChar * )cur->name, (const xmlChar * )"dwml") )
     {   fprintf(stderr, "Fetched wrong file\nNode named \"%s\"", cur->name);
-		xmlFreeDoc(weather);
-		exit(1);
-	}
+        xmlFreeDoc(weather);
+        exit(1);
+    }
 
-	cur = cur->xmlChildrenNode;
+    cur = cur->xmlChildrenNode;
 
-	while ( cur )
+    while ( cur )
     {   if ( !xmlStrcmp((const xmlChar * )cur->name, (const xmlChar * )"head") )
-		{	_parseData(weather, cur);
-		}
+        { _parseData(weather, cur);
+        }
 
-		cur = cur->next;
-	}
+        cur = cur->next;
+    }
 
-	xmlFreeDoc(weather);
+    xmlFreeDoc(weather);
 }
 
 void _parseData(xmlDocPtr weather, xmlNodePtr cur)
 {   xmlChar *datum;
-	cur = cur->xmlChildrenNode;
+    cur = cur->xmlChildrenNode;
 
-	while ( cur )
+    while ( cur )
     {   if ( !xmlStrcmp((const xmlChar * )cur->name, (const xmlChar * )"description") )
-		{	datum = xmlNodeListGetString(weather, cur->xmlChildrenNode, 1);
-			printf("datum: %s\n", datum);
-			xmlFree(datum);
-		}
-		cur = cur->next;
-	}
+        {   datum = xmlNodeListGetString(weather, cur->xmlChildrenNode, 1);
+            printf("datum: %s\n", datum);
+            xmlFree(datum);
+        }
+        cur = cur->next;
+    }
 }
 
 void _formatOutput(char * formatStr)
 {   for ( ; *formatStr; ++formatStr)
     {   if ( *formatStr == '%' )
-		{   if ( *formatStr == '0' ) formatStr++;
+        {   if ( *formatStr == '0' ) formatStr++;
 
-			switch (*++formatStr)
-			{   case '\0':
-				    --formatStr;
-					break;
+            switch (*++formatStr)
+            {   case '\0':
+                    --formatStr;
+                    break;
 
-				case 'c':
-					// condition
-					continue;
+                case 'c':
+                    // condition
+                    continue;
 
-				case 'd':
-					// humidity
-					continue;
+                case 'd':
+                    // humidity
+                    continue;
 
-				case 'D':
-					// dew point
-					continue;
+                case 'D':
+                    // dew point
+                    continue;
 
-				case 'h':
-					// heat index
-					continue;
+                case 'h':
+                    // heat index
+                    continue;
 
-			    case 'H':
-					// hazard warnings
-					continue;
+                case 'H':
+                    // hazard warnings
+                    continue;
 
-				case 'p':
-					// pressure
-					continue;
+                case 'p':
+                    // pressure
+                    continue;
 
-				case 'P':
-					// probability of precipitation
-					continue;
+                case 'P':
+                    // probability of precipitation
+                    continue;
 
-				case 'r':
-					// reporter identity
-					continue;
+                case 'r':
+                    // reporter identity
+                    continue;
 
-				case 'R':
-					// reporter coordinates
-					continue;
+                case 'R':
+                    // reporter coordinates
+                    continue;
 
-				case 't':
-					// temperature
-					continue;
+                case 't':
+                    // temperature
+                    continue;
 
-				case 'v':
-					// visibility
-					continue;
+                case 'v':
+                    // visibility
+                    continue;
 
-				case 'w':
-					// wind speed
-					continue;
+                case 'w':
+                    // wind speed
+                    continue;
 
-				case 'W':
-					// wind direction
-					continue;
+                case 'W':
+                    // wind direction
+                    continue;
 
-				case '%':
-				default:
-					break;
-			}
-		}
-	}
+                case '%':
+                default:
+                    break;
+            }
+        }
+    }
 }
+// vim: set tabstop=4 shiftwidth=4 expandtab
