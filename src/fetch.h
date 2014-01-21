224806760fb0a4822209f43cf14ae4cfddba22e9
Sam Stuewe <halosghost@archlinux.info>
2014-01-20 19:52:45 -0600

    Finish initial separation into headers

---
+++ src/fetch.h
@@ -0,0 +1,86 @@
+#ifndef __FETCH_H__
+#define __FETCH_H__
+
+// Includes //
+#include <curl/curl.h>
+#include "parse.h"
+#include "format.h"
+
+// Variables //
+#define BUFFER_SIZE 80
+
+char formatString[BUFFER_SIZE] = {'\0'};
+char location[BUFFER_SIZE] = {'\0'};
+
+// Prototypes //
+static void _getData (const char * location, const int scale);
+
+// Fetch DWML //
+void _getData (const char * location, const int scale)
+{   CURL * handle;
+    CURLcode res;
+    char url[256] = "";
+    FILE * suppressOutput = fopen("/dev/null", "wb");
+
+    data_t data;
+    memset(&data, '\0', sizeof(data_t));
+
+    curl_global_init(CURL_GLOBAL_ALL);
+    handle = curl_easy_init();
+
+    if ( handle )
+    {   snprintf(url, sizeof(url), "http://forecast.weather.gov/zipcity.php?inputstring=%s", location);
+        curl_easy_setopt(handle, CURLOPT_WRITEDATA, suppressOutput);
+        curl_easy_setopt(handle, CURLOPT_URL, url);
+        curl_easy_setopt(handle, CURLOPT_FOLLOWLOCATION, 1);
+
+        res = curl_easy_perform(handle);
+        if ( res == CURLE_OK )
+        {   char * interim;
+            res = curl_easy_getinfo(handle, CURLINFO_EFFECTIVE_URL, &interim);
+
+            snprintf(url, sizeof(url), "%s&FcstType=dwml&unit=%d", interim, scale);
+
+            if ( res == CURLE_OK )
+            {   curl_easy_setopt(handle, CURLOPT_URL, url);
+                curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, _writeDataToBuffer);
+                curl_easy_setopt(handle, CURLOPT_WRITEDATA, (void * )&data);
+                res = curl_easy_perform(handle);
+            }
+            else
+            {   fprintf(stderr, "Failed to get weather page (%s)\n", curl_easy_strerror(res));
+                fclose(suppressOutput);
+                curl_easy_cleanup(handle);
+                curl_global_cleanup();
+                free(data.buffer);
+                exit(1);
+            }
+        }
+        else
+        {   fprintf(stderr, "Failed to get location (%s)\n", curl_easy_strerror(res));
+            fclose(suppressOutput);
+            curl_easy_cleanup(handle);
+            curl_global_cleanup();
+            free(data.buffer);
+            exit(1);
+        }
+    }
+
+    fclose(suppressOutput);
+    curl_easy_cleanup(handle);
+    curl_global_cleanup();
+
+    //puts(data.buffer);
+
+    _parseDataInBuffer(data.buffer, data.size);
+
+    free(data.buffer);
+
+    if ( *formatString ) _formatOutput(formatString);
+    else
+    {   fprintf(stderr, "No specified format\n");
+        exit(1);
+    }
+}
+
+#endif // __FETCH_H__
--- src/shaman.h
+++ src/shaman.h
@@ -7,35 +7,24 @@
 #include <string.h>
 #include <unistd.h>
 #include <ctype.h>
-#include <curl/curl.h>
 #include <getopt.h>
-#include <libxml/parser.h>
-#include <libxml/tree.h>
+
 #include "usage.h"
 #include "memsafety.h"
-
-// Variables //
-#define BUFFER_SIZE 80
-
-char formatString[BUFFER_SIZE] = {'\0'};
-char location[BUFFER_SIZE] = {'\0'};
-
-typedef struct 
-{   char * buffer;
-    size_t size;
-    size_t offset;
-} data_t;
+#include "fetch.h"
+#include "format.h"
+#include "parse.h"
 
 // Prototypes //
+extern void _usage (void);
 extern void * _memoryAbort (void);
 extern void * _malloc (size_t size);
 extern void * _realloc (void * ptr, size_t size);
-extern void _usage (void);
-static void _getData (const char * location, const int scale);
-static size_t _writeDataToBuffer (char * ptr, size_t size, size_t nmemb, void * userdata);
-static void _parseDataInBuffer (const char * buffer, int buffer_size);
-static void _parseData (xmlDocPtr weather, xmlNodePtr cur);
-static void _parseFormat (char * formatStr);
-static void _formatOutput (char * formatStr);
+extern void _getData (const char * location, const int scale);
+extern size_t _writeDataToBuffer (char * ptr, size_t size, size_t nmemb, void * userdata);
+extern void _parseDataInBuffer (const char * buffer, int buffer_size);
+extern void _parseData (xmlDocPtr weather, xmlNodePtr cur);
+extern void _parseFormat (char * formatString);
+extern void _formatOutput (char * formatString);
 
 #endif // __SHAMAN_H__
