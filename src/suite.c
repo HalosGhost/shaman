c5aba99ee0496eff1fede0f7d556d4cd0225296f
HalosGhost <halosghost@archlinux.info>
2014-06-14 16:32:13 -0500

    much more thorough unit tests

--- src/suite.c
+++ src/suite.c
@@ -36,7 +36,7 @@ int main (void) {
     if ( !test1_result->data ) { return 1; };
 
     int test2_result = test_local_parsing(test1_result);
-    if ( test2_result ) { return 2; };
+    if ( test2_result ) { return test2_result; };
 
     struct json_write_result * test3_result = test_remote_fetching();
     if ( !test1_result->data ) { return 3; };
@@ -64,6 +64,26 @@ int test_local_parsing (struct json_write_result * test) {
     struct weather * weather = read_weather(test);
     int failed_test_counter = 0;
 
+    if ( strcmp(weather->country, "US") != 0 ) { failed_test_counter ++; };
+    if ( strcmp(weather->name, "Georgetown") != 0 ) { failed_test_counter ++; };
+    if ( weather->latitude != 30.63 ) { failed_test_counter ++; };
+    if ( weather->longitude != -97.68 ) { failed_test_counter ++; };
+    if ( weather->sunrise != 1402486044 ) { failed_test_counter ++; };
+    if ( weather->sunset != 1402536816 ) { failed_test_counter ++; };
+    if ( weather->weather_code != 800 ) { failed_test_counter ++; };
+    if ( weather->temperature != 306.35 ) { failed_test_counter ++; };
+    if ( weather->pressure != 1011 ) { failed_test_counter ++; };
+    if ( weather->temp_min != 305.15 ) { failed_test_counter ++; };
+    if ( weather->temp_max != 307.59 ) { failed_test_counter ++; };
+    if ( weather->humidity != 62 ) { failed_test_counter ++; };
+    if ( weather->wind_speed != 1.54 ) { failed_test_counter ++; };
+    if ( weather->wind_gust != 5.14 ) { failed_test_counter ++; };
+    if ( weather->wind_direction != 214 ) { failed_test_counter ++; };
+    if ( weather->precipitation_3h != 18 ) { failed_test_counter ++; };
+    if ( weather->clouds != 18 ) { failed_test_counter ++; };
+    if ( weather->dt != 1402513288 ) { failed_test_counter ++; };
+    if ( weather->id != 4693342 ) { failed_test_counter ++; };
+
     printf("Testing local JSON Parsing\t[ %s ]\n", (failed_test_counter == 0 ? "PASS" : "FAIL"));
 
     free(weather->name);
@@ -88,6 +108,9 @@ int test_remote_parsing (struct json_write_result * test) {
     printf("Testing remote JSON Parsing\t[ PEND ]\r");
     struct weather * weather = read_weather(test);
     int failed_test_counter = 0;
+
+    if ( strcmp(weather->country, "US") != 0 ) { failed_test_counter++; };
+    if ( strcmp(weather->name, "Saint Paul") != 0 ) { failed_test_counter++; };
 
     printf("Testing remote JSON Parsing\t[ %s ]\n", (failed_test_counter == 0 ? "PASS" : "FAIL"));
 
