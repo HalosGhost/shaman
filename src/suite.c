39e0555fb581e2fceb6010e19c44890282bc00bd
HalosGhost <halosghost@archlinux.info>
2014-06-14 15:44:20 -0500

    Begin push for more serious test suite

--- src/suite.c
+++ src/suite.c
@@ -24,19 +24,77 @@
 #include <string.h>
 #include "weather.h"
 
-// Test Function //
-int main (void) {
-    printf("Fetching JSON\t[ PEND ]\r");
-    struct json_write_result * test = fetch_data_owm('q', "Saint%20Paul,us", 'i');
-    printf("Fetching JSON\t[ %s ]\n", (test->data ? "PASS" : "FAIL"));
+// Forward Declarations //
+struct json_write_result * test_local_fetching (void);
+int test_local_parsing (struct json_write_result * test);
+struct json_write_result * test_remote_fetching (void);
+int test_remote_parsing (struct json_write_result * test);
 
-    printf("Reading JSON\t[ PEND ]\r");
+// Run Suite //
+int main (void) {
+    struct json_write_result * test1_result = test_local_fetching();
+    if ( !test1_result->data ) { return 1; };
+
+    int test2_result = test_local_parsing(test1_result);
+    if ( test2_result ) { return 2; };
+
+    struct json_write_result * test3_result = test_remote_fetching();
+    if ( !test1_result->data ) { return 3; };
+
+    int test4_result = test_remote_parsing(test3_result);
+    if ( test4_result ) { return 4; };
+
+    return 0;
+}
+
+struct json_write_result * test_local_fetching (void) {
+    printf("Testing local JSON Fetching\t[ PEND ]\r");
+    struct json_write_result * test = fetch_data_file("/home/halosghost/Projects/shaman/src/test.json");
+    printf("Testing local JSON Fetching\t[ %s ]\n", (test->data ? "PASS" : "FAIL"));
+
+    if ( test->data ) {
+        return test;
+    } else {
+        return NULL;
+    }
+}
+
+int test_local_parsing (struct json_write_result * test) {
+    printf("Testing local JSON Parsing\t[ PEND ]\r");
     struct weather * weather = read_weather(test);
-    printf("Reading JSON\t[ %s ]\n", (strcmp("Saint Paul", weather->name) ? "PASS" : "FAIL"));
+    int failed_test_counter = 0;
+
+    printf("Testing local JSON Parsing\t[ %s ]\n", (failed_test_counter == 0 ? "PASS" : "FAIL"));
 
     free(weather->name);
     free(weather->country);
-    return 0;
+
+    return failed_test_counter;
+}
+
+struct json_write_result * test_remote_fetching (void) {
+    printf("Testing remote JSON Fetching\t[ PEND ]\r");
+    struct json_write_result * test = fetch_data_owm('q', "Saint%20Paul,us", 'i');
+    printf("Testing remote JSON Fetching\t[ %s ]\n", (test->data ? "PASS" : "FAIL"));
+
+    if ( test->data ) {
+        return test;
+    } else {
+        return NULL;
+    }
+}
+
+int test_remote_parsing (struct json_write_result * test) {
+    printf("Testing remote JSON Parsing\t[ PEND ]\r");
+    struct weather * weather = read_weather(test);
+    int failed_test_counter = 0;
+
+    printf("Testing remote JSON Parsing\t[ %s ]\n", (failed_test_counter == 0 ? "PASS" : "FAIL"));
+
+    free(weather->name);
+    free(weather->country);
+
+    return failed_test_counter;
 }
 
 // vim: set ts=4 sw=4 et:
