cd0cb2b55b08930110b185953563c4af277acb77
HalosGhost <halosghost@archlinux.info>
2014-06-16 00:38:50 -0500

    begin work on initial planned feature set

--- src/shaman.c
+++ src/shaman.c
@@ -24,17 +24,100 @@
 #include <string.h>
 #include <getopt.h>
 #include "weather.h"
+#include "usage.h"
 
 // Main Function //
 int main (int argc, char ** argv) {
-//    struct json_write_result * test = fetch_data_file("./test/test.json");
-    struct json_write_result * test = fetch_data_owm('q', "Saint%20Paul,US", 'i', NULL);
-    struct weather * weather = read_weather(test);
-	char test_string [1024] = {'\0'};
+    char flag_scale = 'i';
+    char flag_refresh = 0;
+    char * format;
+    char * location;
+    char * cache_path = NULL;
 
-	strfweather(test_string, 1024, "%a (%t°F)", weather);
+    if ( argc <= 1 ) { 
+        _usage(1); 
+    } else {
+        int c;
 
-	printf("%s\n", test_string);
+        while ( c != -1 ) {
+            static struct option options [] = {
+                /* Flags */
+                { "help",     no_argument,         0, 'h'   },
+                { "imperial", no_argument,         0, 'i'   },
+                { "metric",   no_argument,         0, 'm'   },
+                { "refresh",  no_argument,         0, 'r'   },
+                /* Swtiches */
+                { "cache",    required_argument,   0, 'c'   },
+                { "format",   required_argument,   0, 'f'   },
+                { "location", required_argument,   0, 'l'   },
+                { 0,          0,                   0, 0     },
+            };
+
+            int option_index = 0;
+
+            c = getopt_long(argc, argv, "himc:f:l:", options, &option_index);
+            switch ( c ) {
+                case 'h':
+                    _usage(0);
+                    break;
+
+                case 'i':
+                    flag_scale = 'i';
+                    break;
+
+                case 'm':
+                    flag_scale = 'm';
+                    break;
+
+                case 'x':
+                    flag_refresh ++;
+                    break;
+
+                case 'c': {
+                    size_t cache_path_length = strlen(optarg) + 1;
+                    cache_path = malloc(cache_path_length);
+                    snprintf(cache_path, cache_path_length, "%s", optarg);
+                    break;
+                }
+
+                case 'f': {
+                    size_t format_length = strlen(optarg) + 1;
+                    format = malloc(format_length);
+                    strncpy(format, optarg, format_length);
+                    break;
+                }
+
+                case 'l': {
+                    // TODO: Correctly encode location
+                    size_t location_length = strlen(optarg) + 1;
+                    location = malloc(location_length);
+                    snprintf(location, location_length, "%s", optarg);
+                    break;
+                }
+            }
+        }
+    }
+
+    struct json_write_result * json;
+
+    if ( !cache_path ) {
+        // TODO: Correctly check for default cache
+        json = fetch_data_owm('q', location, flag_scale, cache_path);
+    } else {
+        // TODO: Handle cache freshening
+        json = fetch_data_file(cache_path);
+    }
+
+    if ( cache_path ) { free(cache_path); };
+    if ( location ) { free(location); };
+
+    struct weather * weather = read_weather(json);
+    char output_string [1024] = {'\0'};
+
+    strfweather(output_string, 1024, format, weather);
+    if ( format ) { free(format); };
+
+    printf("%s\n", output_string);
 
     return 0;
 }
