d45c08423552532a0baf9c11c4d8bfd850929b34
HalosGhost <halosghost@archlinux.info>
2014-11-19 08:18:26 -0600

    Add color (\o/); Align output with test suite; add compiler check

--- configure.sh
+++ configure.sh
@@ -1,23 +1,32 @@
 #!/usr/bin/env bash
 
 check_lib () {
-    printf 'Checking for %s\t\t\t\t[ PEND ]\r' "$1"
+    printf 'Checking for %s\t\t\t[ PEND ]\r' "$1"
     if [[ "$(pkg-config --exists $1; echo $?)" == 0 ]]; then
-        printf 'Checking for %s\t\t\t\t[ PASS ]\n' "$1"
+        printf 'Checking for %s\t\t\t[ \x1b[32mPASS\x1b[0m ]\n' "$1"
     else
-        printf 'Checking for %s\t\t\t\t[ FAIL ]\n' "$1"
+        printf 'Checking for %s\t\t\t[ \x1b[31mFAIL\x1b[0m ]\n' "$1"
+        return 1
     fi
 }
 
 check_header () {
-    printf 'Checking for %s\t\t[ PEND ]\r' "$1"
+    printf 'Checking for %s\t[ PEND ]\r' "$1"
     if [[ -f "$1" ]]; then
-        printf 'Checking for %s\t\t[ PASS ]\n' "$1"
+        printf 'Checking for %s\t[ \x1b[32mPASS\x1b[0m ]\n' "$1"
     else
-        printf 'Checking for %s\t\t[ FAIL ]\n' "$1"
+        printf 'Checking for %s\t[ \x1b[31mFAIL\x1b[0m ]\n' "$1"
         return 1
     fi
 }
+
+printf 'Checking for /usr/bin/clang\t\t[ PEND ]\r'
+if [[ -f '/usr/bin/clang' ]]; then
+    printf 'Checking for /usr/bin/clang\t\t[ \x1b[32mPASS\x1b[0m ]\n'
+else
+    printf 'Checking for /usr/bin/clang\t\t[ \x1b[31mFAIL\x1b[0m ]\n'
+    return 1
+fi
 
 for i in "libcurl" "jansson"; do
     check_lib "$i"
